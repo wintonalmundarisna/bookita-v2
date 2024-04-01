f9a249de34fe2c267159b2219b7f8a7e95bf61f4
DeniArdiyan <deniardiyansyah14@gmail.com>
2024-04-01 15:24:49 +0700

    login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -9,17 +9,18 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
         <link rel="stylesheet" href="app.css">
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap" rel="stylesheet">
+
     {{-- Vite buat hot reload --}}
     @vite([])
-
-    {{-- icon bootstrap --}}
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <title>Login</title>
-    <style>
-  body{
+</head>
+
+<style>
+   body{
     font-family: "Poppins", sans-serif;
-  font-weight: 300;
-  font-style: normal;
+    font-weight: 300;
+    font-style: normal;
   }
     .btn{
         background-color: #FF5D0A;
@@ -31,10 +32,10 @@
     }
     .form-control{
         border: none;
-  border-bottom: 2px solid #000842;
-  outline: none;
-  background: transparent;
-  border-radius: 0;
+    border-bottom: 2px solid #000842;
+    outline: none;
+    background: transparent;
+    border-radius: 0;
 
     }
     .form-label{
@@ -43,46 +44,52 @@
     .h2{
       color: #FF5D0A;
     }
+
 </style>
-    <head>
+
+<body>
     <section class="vh-100 pt-3">
         <div class="container-fluid">
           <div class="row">
             <div class="col-sm-6 text-black">
-                <div class="col">
+      
+            <div class="col">
                     <img src="logo.png" class="w-25" alt="">
                 </div>
-                <div class="px-5 ms-xl-4">
-                <i class="fas fa-crow fa-2x me-3 pt-5 mt-xl-4" style="color: #709085;"></i>
-              </div>
-                    <form>
-                    <h3 class="fw-bold mb-3 pb-3" style="letter-spacing: 1px;">Sign Up</h3>
-                    <p>if you don't have an account register you can <a href="#!" class="link-warning">register here !</a></p>
-                        <div class="mb-3">
-                            <label for="exampleInputEmail1" class="form-label">Email</label>
-                            <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter you email address"></input>
-                        </div>
-                        <div class="mb-3">
-                            <label for="exampleInputPassword1" class="form-label">Password</label>
-                            <input placeholder="Enter you password" type="password" class="form-control" id="exampleInputPassword1">
-                        </div>
-                        <div class="pt-1 mb-4">
+      
+              <div class="d-flex align-items-center h-custom-2 px-5 ms-xl-4 mt-5 pt-5 pt-xl-0 mt-xl-n5">
+      
+                <form style="width: 23rem;">
+      
+                  <h3 class="fw-bold mb-3 pb-3" style="letter-spacing: 1px;">Sign in</h3>
+                  <p>if you don't have an account register</p>
+                        <p>you can <a href="#!" class="link-warning">Register here !</a></p>
+                  <div class="form-outline mb-1">
+                  <label for="exampleInputEmail1" class="form-label fw-bold mt-3">Email</label>
+                            <input type="email" class="form-control" id="exampleInputEmail1"
+                             placeholder="Enter you email address"></input>
+                  </div>
+                  <div class="form-outline mb-1">
+                  <label for="exampleInputPassword1" class="form-label fw-bold mt-3">Password</label>
+                            <input placeholder="Enter you password" type="password" 
+                            class="form-control" id="exampleInputPassword1">
+                  </div>
+                  <div class="pt-1 mb-4">
                     <button class="btn btn-warning btn-md rounded-pill fw-bold" type="button">Login</button>
                   </div>
                   <div class="text-center">
                       <span class="d-line">or continue white</span>
                 </div>
-                    </form>
-                </div>
-                <div class="col">
-                    <img src="login.png" class="w-100" alt="">
-                </div>
+                </form>
+              </div>
             </div>
+            <div class="col-sm-6 py-10 d-none d-sm-block pt-5">
+              <img  src="login.png"
+                alt="Login" class="w-100 vh-95" >
+            </div>
+          </div>
         </div>
-    </head>
-
-<body>
-
+      </section>
 
     {{-- cdn bootstrap --}}
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
