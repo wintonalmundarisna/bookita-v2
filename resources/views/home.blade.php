004f50da1c64fc50d82c7a6cc42f791ccde4b5b8
wintonalmundarisna <wintonalmundarisna@gmail.com>
2024-04-29 12:10:35 +0700

    Fixing

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -206,7 +206,7 @@
 <body>
     <nav class="navbar navbar-expand-lg bg-white py-4">
         <div class="container-md">
-            <a class="navbar-brand" href="#"><img src="bookita.PNG" alt="" class="navbar-image"></a>
+            <a class="navbar-brand" href="/home"><img src="bookita.PNG" alt="" class="navbar-image"></a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                 data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
                 aria-label="Toggle navigation">
--- resources/views/pageBaca.blade.php
+++ resources/views/pageBaca.blade.php
@@ -15,10 +15,11 @@
     <title>Home</title>
 
     <style>
-        body{
+        body {
             padding: 0;
             margin: 0;
         }
+
         @media (max-width: 576px) {
             .logout {
                 margin-top: 15px;
@@ -66,16 +67,16 @@
             }
         }
 
-       .image-baca{
-        width: 100%;
-  max-width: 70%;
-  height: auto;
-       }
+        .image-baca {
+            width: 100%;
+            max-width: 70%;
+            height: auto;
+        }
 
-       .fillContent{
-        font-size: 2 vw;
-        padding: 50px 100px 50px 100px;
-       }
+        .fillContent {
+            font-size: 2 vw;
+            padding: 50px 100px 50px 100px;
+        }
     </style>
 </head>
 
@@ -128,27 +129,36 @@
 
         <h1 class="text-center pt-5">The Butcher And The Wren</h1>
 
-        
-        
+
+
     </section>
     <div class="fillContent">
         <p class="first">
-            Kegelapan yang pekat semakin mendekat. Mengisahkan tentang kehidupan seorang pemburu hantu legendaris yang menangkap dan memusnahkan para hantu setiap ada kesempatan. Kegelapan yang pekat semakin mendekat. Mengisahkan tentang kehidupan seorang pemburu hantu legendaris yang menangkap dan memusnahkan para hantu setiap ada kesempatan. Kegelapan yang pekat semakin mendekat. Mengisahkan tentang kehidupan seorang pemburu hantu legendaris yang menangkap dan memusnahkan para hantu setiap ada kesempatan
+            Kegelapan yang pekat semakin mendekat. Mengisahkan tentang kehidupan seorang pemburu hantu legendaris yang
+            menangkap dan memusnahkan para hantu setiap ada kesempatan. Kegelapan yang pekat semakin mendekat.
+            Mengisahkan tentang kehidupan seorang pemburu hantu legendaris yang menangkap dan memusnahkan para hantu
+            setiap ada kesempatan. Kegelapan yang pekat semakin mendekat. Mengisahkan tentang kehidupan seorang pemburu
+            hantu legendaris yang menangkap dan memusnahkan para hantu setiap ada kesempatan
         </p>
         <p class="second">
-            Kegelapan yang pekat semakin mendekat. Mengisahkan tentang kehidupan seorang pemburu hantu legendaris yang menangkap dan memusnahkan para hantu setiap ada kesempatan.
+            Kegelapan yang pekat semakin mendekat. Mengisahkan tentang kehidupan seorang pemburu hantu legendaris yang
+            menangkap dan memusnahkan para hantu setiap ada kesempatan.
         </p>
         <p class="third">
-            Kegelapan yang pekat semakin mendekat. Mengisahkan tentang kehidupan seorang pemburu hantu legendaris yang menangkap dan memusnahkan para hantu setiap ada kesempatan.
+            Kegelapan yang pekat semakin mendekat. Mengisahkan tentang kehidupan seorang pemburu hantu legendaris yang
+            menangkap dan memusnahkan para hantu setiap ada kesempatan.
         </p>
         <p class="fourth">
-            Kegelapan yang pekat semakin mendekat. Mengisahkan tentang kehidupan seorang pemburu hantu legendaris yang menangkap dan memusnahkan para hantu setiap ada kesempatan.
+            Kegelapan yang pekat semakin mendekat. Mengisahkan tentang kehidupan seorang pemburu hantu legendaris yang
+            menangkap dan memusnahkan para hantu setiap ada kesempatan.
         </p>
         <p class="fifth">
-            Kegelapan yang pekat semakin mendekat. Mengisahkan tentang kehidupan seorang pemburu hantu legendaris yang menangkap dan memusnahkan para hantu setiap ada kesempatan. Kegelapan yang pekat semakin mendekat. Mengisahkan tentang kehidupan seorang.
+            Kegelapan yang pekat semakin mendekat. Mengisahkan tentang kehidupan seorang pemburu hantu legendaris yang
+            menangkap dan memusnahkan para hantu setiap ada kesempatan. Kegelapan yang pekat semakin mendekat.
+            Mengisahkan tentang kehidupan seorang.
         </p>
     </div>
-    
+
     <div class="position-relative bg-dark  row">
 
         <div class="text-footer">
@@ -156,66 +166,66 @@
         </div>
         <div class="contact">
 
-       
 
-    <div class="container-fluid mt-5 text-white" style="background-color: #303030;">
-        <div class="container-xl foot">
-            <div class="row" style="padding-block: 10%;">
-                <div class="col-4" style="text-align: start">
-                    <h1 class="jadikan" style="">Jadikan bacaanmu menyenangkan bersama booKita</h1>
-                </div>
-                <div class="col-3 d-flex justify-content-end">
-                    <ul class="address">
-                        <li class="mb-3" style="list-style: none;">
-                            <h5 class="bab">Address</h5>
-                        </li>
-                        <li style="list-style: none">
-                            <p>Jalan Nangka</p>
-                        </li>
-                        <li style="list-style: none;">
-                            <p>Jaksel</p>
-                        </li>
-                        <li style="list-style: none">
-                            <p>Indonesia</p>
-                        </li>
-                        <li style="list-style: none;">
-                            <div class="row" style="width: max-content">
-                                <div class="col">
-                                    <i class="bi bi-facebook"></i>
-                                </div>
-                                <div class="col">
-                                    <i class="bi bi-twitter"></i>
-                                </div>
-                                <div class="col">
-                                    <i class="bi bi-youtube"></i>
-                                </div>
+
+            <div class="container-fluid mt-5 text-white" style="background-color: #303030;">
+                <div class="container-xl foot">
+                    <div class="row" style="padding-block: 10%;">
+                        <div class="col-4" style="text-align: start">
+                            <h1 class="jadikan" style="">Jadikan bacaanmu menyenangkan bersama booKita</h1>
+                        </div>
+                        <div class="col-3 d-flex justify-content-end">
+                            <ul class="address">
+                                <li class="mb-3" style="list-style: none;">
+                                    <h5 class="bab">Address</h5>
+                                </li>
+                                <li style="list-style: none">
+                                    <p>Jalan Nangka</p>
+                                </li>
+                                <li style="list-style: none;">
+                                    <p>Jaksel</p>
+                                </li>
+                                <li style="list-style: none">
+                                    <p>Indonesia</p>
+                                </li>
+                                <li style="list-style: none;">
+                                    <div class="row" style="width: max-content">
+                                        <div class="col">
+                                            <i class="bi bi-facebook"></i>
+                                        </div>
+                                        <div class="col">
+                                            <i class="bi bi-twitter"></i>
+                                        </div>
+                                        <div class="col">
+                                            <i class="bi bi-youtube"></i>
+                                        </div>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="col-5 text-white" style="text-align: end">
+                            <div class="row">
+                                <h5 class="mb-3 bab">Kontak</h5>
                             </div>
-                        </li>
-                    </ul>
-                </div>
-                <div class="col-5 text-white" style="text-align: end">
-                    <div class="row">
-                        <h5 class="mb-3 bab">Kontak</h5>
-                    </div>
-                    <div class="row mb-3 d-flex justify-content-end" style="">
-                        <button type="btn" class="btn px-4 py-2 text-white bab"
-                            style="background-color: #F1592B; width: max-content;">bookita@gmail.com</button>
-                    </div>
-                    <div class="row">
-                        <h5 class="fw-semibold bab">+ 62 878 206 255 14</h5>
+                            <div class="row mb-3 d-flex justify-content-end" style="">
+                                <button type="btn" class="btn px-4 py-2 text-white bab"
+                                    style="background-color: #F1592B; width: max-content;">bookita@gmail.com</button>
+                            </div>
+                            <div class="row">
+                                <h5 class="fw-semibold bab">+ 62 878 206 255 14</h5>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </div>
-    <div class="container-fluid bg-dark text-white p-3" style="text-align: center">
-        <h6 class="fw-light">All Right Reserverd. Copyright © booKita 2024 </h6>
-    </div>
-    
-    {{-- cdn bootstrap --}}
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
-    </script>
+            <div class="container-fluid bg-dark text-white p-3" style="text-align: center">
+                <h6 class="fw-light">All Right Reserverd. Copyright © booKita 2024 </h6>
+            </div>
+
+            {{-- cdn bootstrap --}}
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+                integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+            </script>
 </body>
 
 </html>
